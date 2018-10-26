66b793884b96da24dd8db630e54b0141e8a6b814
Shrinr <sriram.narayanan@gmail.com>
2018-10-26 10:55:55 -0700

    hello world
    hello world

---
+++ Sample2.py
@@ -0,0 +1,4 @@
+x = 3;
+y = 2;
+z="Hope this works"
+print "Test this",x*y,z
---
+++ Sample4.py
@@ -0,0 +1,11 @@
+#x = "Dont know what the fuck i am doing at %d" %25
+import io
+inputname = raw_input('Enter the filename : ')
+InputText = "Test Hello"
+#print(Filename)
+#Filepath = "C:\Shri - Personal\Python\Files"
+#Filename = "Test1.txt"
+open(inputname, "a")
+inputname.write("InputText")
+inputname.write(x)
+inputname.close()
